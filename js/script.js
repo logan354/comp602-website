e2d828d04db2429301b678f87f8896eed3907fb5
Ikatere <raine.roberts06@gmail.com>
2023-09-30 16:48:43 +1300

    product page + updated links on shop

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+
+/* Index Page, Categories */
+function redirectToPage(pageUrl) {
+    window.location.href = 'shop.html';
+}
+
+
+/* Shop Page, Flaoting Menu*/
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.menu-toggle');
     const floatingMenu = document.querySelector('.floating-menu');
@@ -6,3 +14,37 @@ document.addEventListener('DOMContentLoaded', function() {
         floatingMenu.classList.toggle('active');
     });
 });
+
+/* Product Page */
+document.addEventListener('DOMContentLoaded', function() {
+    const productDisplay = document.querySelector('.product-display');
+    const productImage = productDisplay.querySelector('.product-image');
+    const prevButton = productDisplay.querySelector('.prev-button');
+    const nextButton = productDisplay.querySelector('.next-button');
+    
+    const images = [
+      'images/product/p1.png',
+      'images/product/p1var/p1a.png',
+      'images/product/p1var/p1b.png',
+      'images/product/p1var/p1c.png'
+    ];
+    
+    let currentImageIndex = 0;
+    
+    function updateImage() {
+      productImage.src = images[currentImageIndex];
+    }
+    
+    prevButton.addEventListener('click', function() {
+      currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+      updateImage();
+    });
+    
+    nextButton.addEventListener('click', function() {
+      currentImageIndex = (currentImageIndex + 1) % images.length;
+      updateImage();
+    });
+    
+    updateImage();
+  });
+  
